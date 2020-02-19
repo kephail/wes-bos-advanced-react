6ce80aeef83971e75efc2bfc541bc86fecd018b3
Peter Jones <kephail@me.com>
2020-02-19 17:18:49 +0000

    Progress up until the end of video 14

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,20 @@
-const Mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
+
+    console.log(info);
+
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args,
+        },
+      },
+      info,
+    );
+
+    return item;
+  },
+};
 
 module.exports = Mutations;
